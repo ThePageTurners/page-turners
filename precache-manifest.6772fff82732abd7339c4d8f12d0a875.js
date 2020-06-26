52b04d00a8c9bd56662ab46aae7b48914aff9c66
David Downie <daviddownie@gmail.com>
2020-06-26 14:18:28 -0400

    Updates

---
+++ precache-manifest.6772fff82732abd7339c4d8f12d0a875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5e6f149f66ec52b8be8ac83875b82b",
+    "url": "/page-turners/index.html"
+  },
+  {
+    "revision": "a25240ba40bbeb13791f",
+    "url": "/page-turners/static/css/main.3d7f450b.chunk.css"
+  },
+  {
+    "revision": "da507fea73d127349627",
+    "url": "/page-turners/static/js/2.22f8d842.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/page-turners/static/js/2.22f8d842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25240ba40bbeb13791f",
+    "url": "/page-turners/static/js/main.1d3d00e2.chunk.js"
+  },
+  {
+    "revision": "ec09ec2ebfe68f04c455",
+    "url": "/page-turners/static/js/runtime-main.2be28b97.js"
+  }
+]);
