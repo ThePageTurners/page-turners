025aa8fb2d5d27e1061206b7f6dbda42de186d0d
David Downie <daviddownie@gmail.com>
2020-06-28 20:26:30 -0400

    Added book item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.scss';
-import firebase from './Firebase/index.js';
-import bookPlaceHolder from './assets/bookPlaceholder.png'
-// import BookItem from './components/BookItem.js'
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.scss";
+import firebase from "./Firebase/index.js";
+import BookItem from "./components/BookItem.js";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			books: [],
-			userInput: '',
+			userInput: "",
 			bookShelf: []
 		};
 	}
 
 	componentDidMount() {
 		// For retrieval of bookshelf list***
-		const dbRef = firebase.database().ref('readingList');
+		const dbRef = firebase.database().ref("readingList");
 		dbRef.on('value', (snapshot) => {
 			const readingList = [];
 			const data = snapshot.val();
@@ -41,7 +40,7 @@ class App extends Component {
 			method: 'GET',
 			responseType: 'JSON',
 			params: {
-				key: 'AIzaSyD7Ytli5GUZu5S7FoaFn-sSMzsdWuwv_8E',
+				key: "AIzaSyD7Ytli5GUZu5S7FoaFn-sSMzsdWuwv_8E",
 				q: searchItem
 			}
 		}).then((response) => {
@@ -63,12 +62,12 @@ class App extends Component {
 		let searchTerm = this.state.userInput;
 		this.findBooks(searchTerm);
 		this.setState({
-			userInput: ''
+			userInput: ""
 		})
 	};
 
 	handleClickAdd = (index) => {
-		const dbRef = firebase.database().ref('readingList');
+		const dbRef = firebase.database().ref("readingList");
 		const objectPush = {
 			title: this.state.books[index].volumeInfo.title,
 			author: this.state.books[index].volumeInfo.authors,
@@ -77,12 +76,12 @@ class App extends Component {
 	}
 	
 	handleKeyPress = (e) => {
-		if (e.key === 'Enter') {
+		if (e.key === "Enter") {
 			if (!this.state.userInput) return;
 			let searchTerm = this.state.userInput;
 			this.findBooks(searchTerm);
 			this.setState({
-				userInput: ''
+				userInput: ""
 			})
 		}
 	};
@@ -105,46 +104,24 @@ class App extends Component {
 				<h1>Books!</h1>
 				<ul>
 					{this.state.books.map((book, index) => {
-						// let authorName = book.volumeInfo.authors;
-						// let authorNameAsString = authorName.join(', ');
-						// <p>{authorNameAsString}</p>
+						let searchedBook = book.volumeInfo;
 						return (
-								<div key={index}>
-								<li>
-									<p>{book.volumeInfo.title}</p>
-									<p>{book.volumeInfo.authors}</p>
-									<p>{book.volumeInfo.description}</p>
-									<p>Genre: {book.volumeInfo.categories}</p>
-									<p>Rating: {book.volumeInfo.averageRating}</p>
-									{
-									book.volumeInfo.imageLinks === undefined ?
-									 (<img src={bookPlaceHolder} alt={book.volumeInfo.title} />)
-									 : 
-									(<img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />)
-									}
-									<button onClick={()=>this.handleClickAdd(index)}>Add Book</button>
-									{/* <image src={
-											book.volumeInfo.imageLinks === undefined
-											  ? {bookPlaceHolder}
-											  : `${book.volumeInfo.imageLinks.thumbnail}`
-									}/> */}
-								</li>
-								
-								{/* <li>
-                                    <BookItem
-                                        key={index}
-                                        title={book.volumeInfo.title}
-                                        authors={book.volumeInfo.authors}
-                                        description={book.volumeInfo.description}
-										thumbnail={book.volumeInfo.imageLinks.thumbnail}
-										imageLinks={book.volumeInfo.imageLinks}
-                                    />
-                                </li> */}
-							</div>
+							<li key={index}>
+								<BookItem
+									key={index}
+									title={searchedBook.title}
+									authors={searchedBook.authors}
+									description={searchedBook.description}
+									genre={searchedBook.categories}
+									rating={searchedBook.averageRating}
+									thumbnail={searchedBook.imageLinks.thumbnail}
+									imageLinks={searchedBook.imageLinks}
+									handleClickAdd={this.handleClickAdd}
+								/>
+							</li>
 						);
 					})}
 				</ul>
-				{/* <button onClick={() => this.deleteCharacter(character.id)}>Delete</button> */}
 			</div>
 		);
 	}
--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import bookPlaceHolder from '../assets/bookPlaceholder.png'
+import React from "react"
+import bookPlaceHolder from "../assets/bookPlaceholder.png"
 
 const BookItem = (props) => {
     return (
         <div>
             <h2>{props.title}</h2>
-            { props.authors.length > 1 ? (
-            <p className="author">{props.authors.join(', ')}</p>
+            { props.authors && props.authors.length > 1 ? (
+            <p className="author">{props.authors.join(", ")}</p>
             ) :
             (<p className="author">{props.authors}</p>)}
-            {/* <p>{authorNameAsString}</p> */}
-            {/* <p>{book.volumeInfo.authors}</p> */}
-            <p className='bookDescription'>{props.description}</p>
-            {
-            !props.imageLinks
-            ?
+            <p className="bookDescription">{props.description}</p>
+            <p>Genre: {props.genre}</p>
+			<p>Rating: {props.rating}</p>
+
+            { !props.imageLinks ?
 			(<img src={bookPlaceHolder} alt={props.title} />)
-			:
+			: 
 			(<img src={props.thumbnail} alt={props.title} />)
 			}
-            <button>Add Book</button>
+			<button onClick={()=>props.handleClickAdd(props.index)}>Add Book</button>
         </div>
     )
 }
