5a7d062b680e02141f35117ff4d32c371a343df6
David Downie <daviddownie@gmail.com>
2020-06-29 22:05:13 -0400

    changed routing for account component

--- src/App.js
+++ src/App.js
@@ -1,153 +1,48 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 // * * * * ROUTER ELEMENTS
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Welcome from './components/landing';
-import Account from './Account/index';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Account from "./components/Account";
 // * * * * ROUTER ELEMENTS END
 
-import axios from "axios";
-import firebase from "./Firebase/index.js";
-import BookItem from "./components/BookItem.js";
-
-import Bookshelf from "./components/Bookshelf.js";
-import bookPlaceHolder from "./assets/bookPlaceholder.png";
-import githubLogo from './assets/github.svg';
+import githubLogo from "./assets/github.svg";
 import "./App.scss";
 
-const Home = () => {
-  return <h2>I’M THE HOME PAGE</h2>;
-};
-
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     books: [],
-  //     userInput: "",
-  //     bookShelf: [],
-  //     readingList: [],
-  //     imageLinks: bookPlaceHolder,
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   // For retrieval of bookshelf list***
-  //   const dbRef = firebase.database().ref("readingList");
-  //   dbRef.on("value", (snapshot) => {
-  //     const readingList = [];
-  //     const data = snapshot.val();
-
-  //     for (let key in data) {
-  //       readingList.push({
-  //         id: key,
-  //         bookList: data[key],
-  //       });
-  //     }
-  //     this.setState({
-  //       bookShelf: readingList,
-  //     });
-  //   });
-  // }
-
-//   findBooks = (searchItem) => {
-//     axios({
-//       url: `https://www.googleapis.com/books/v1/volumes?`,
-//       method: "GET",
-//       responseType: "JSON",
-//       params: {
-//         key: "AIzaSyD7Ytli5GUZu5S7FoaFn-sSMzsdWuwv_8E",
-//         q: searchItem,
-//       },
-//     }).then((response) => {
-//       let books = response.data.items;
-//       this.setState({
-//         books,
-//       })
-//     }).catch((error)=>{
-//       alert(error)
-//       this.setState({
-//         hasError: false
-//       })
-//     })
-//   };
-
-// const Home = () => {
-//   return (
-//         <h2>I'M THE HOME PAGE</h2>
-//   );
-// };
-
-//   handleClickAdd = (index) => {
-//     const dbRef = firebase.database().ref("readingList");
-
-//     const books = this.state.books;
-
-//     const objectPush = {
-//       title: books[index].volumeInfo.title,
-//       author: books[index].volumeInfo.authors,
-//       description: books[index].volumeInfo.description,
-//       genre: books[index].volumeInfo.categories,
-//       rating: books[index].volumeInfo.averageRating,
-//       imageLinks: books[index].volumeInfo.imageLinks,
-//       isRead: false,
-//     };
-      
-	
-// 	for (let item in objectPush) {
-//     if (objectPush[item] === undefined) {
-//       delete objectPush[item];
-//     }
-//   }
-  
-//   this.setState({
-// 	  readingList: objectPush,
-// 	});
-// 	dbRef.push(objectPush);
-//   };
-
   render() {
-
-  
     return (
       <Router>
         <div className="App">
-
           <header>
             <h1>Page Turners</h1>
 
             <nav className="headerNav">
-              <ul> 
+              <ul>
                 <li>
-                  <Link to="/">Landing</Link>
-                </li> 
-                <li>
-                  <Link to="/home">Home</Link> 
+                  <Link to="/">Home</Link>
                 </li>
+                {/* <li>
+                  <Link to="/home">Home</Link>
+                </li> */}
                 <li>
-                  <Link to="/account">My Reading Lists</Link>
-                </li> 
+                  <Link to="/account">Search Books</Link>
+                </li>
               </ul>
-          </nav> 
-
+            </nav>
           </header>
-          
+
           <main>
             <div className="pageReturn">
-
-              <Route exact path="/" component={Welcome} />
-              <Route exact path="/home" component={Home} />
+              <Route exact path="/" component={Home} />
+              {/* <Route exact path="/home" component={Home} /> */}
               <Route exact path="/account" component={Account} />
-
             </div>
 
-            <div className="pageReturnAside">
-
+            {/* <div className="pageReturnAside">
               <h3>I AM A TITLE</h3>
               <p>Hello</p>
-
-            </div>
-
+            </div> */}
           </main>
 
           <footer>
@@ -155,30 +50,28 @@ class App extends Component {
               <h3>Group Project By:</h3>
               <ul>
                 <li>
-                  <img src={githubLogo} alt="Github Logo"/>
+                  <img src={githubLogo} alt="Github Logo" />
                   <a href="https://github.com/daibhidhdwaum">@daibhidhdwaum</a>
                 </li>
                 <li>
-                  <img src={githubLogo} alt="Github Logo"/>
-                  <a href="https://github.com/vigyan-k">@vigyan-k</a> 
+                  <img src={githubLogo} alt="Github Logo" />
+                  <a href="https://github.com/vigyan-k">@vigyan-k</a>
                 </li>
                 <li>
-                  <img src={githubLogo} alt="Github Logo"/>
+                  <img src={githubLogo} alt="Github Logo" />
                   <a href="https://github.com/OksanaSam">@OksanaSama</a>
                 </li>
                 <li>
-                  <img src={githubLogo} alt="Github Logo"/>
+                  <img src={githubLogo} alt="Github Logo" />
                   <a href="https://github.com/amay-zingg">@amay-zingg</a>
                 </li>
               </ul>
             </div>
-            </footer>
-          
+          </footer>
         </div>
-        </Router>
-          
-
-
-    )}};
+      </Router>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {  Fragment } from "react";
+
+const Home = () => {
+  return (
+      <Fragment>
+        <h2>I'M A Home PAGE</h2>
+        <h2>Welcome</h2>
+        <p>Hello! Find the books you want to read using Page Turners and keep your list as a reference.</p>
+        </Fragment>
+  );
+};
+
+export default Home;
