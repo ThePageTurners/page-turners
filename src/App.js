6c9776ed22637016981ae8130e1460f30db6829f
amay-zingg <anastasiamay29@gmail.com>
2020-06-29 20:43:32 -0400

    Added Npm install react-router-dom And added some basic stylings and the router to go through 3 pages

--- src/App.js
+++ src/App.js
@@ -1,151 +1,108 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+
+// * * * * ROUTER ELEMENTS
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Welcome from './components/landing';
+import Account from './Account/index';
+// * * * * ROUTER ELEMENTS END
+
 import axios from "axios";
-import "./App.scss";
 import firebase from "./Firebase/index.js";
 import BookItem from "./components/BookItem.js";
+// import Bookshelf from "./components/Bookshelf.js";
+import bookPlaceHolder from "./assets/bookPlaceholder.png";
+import githubLogo from './assets/github.svg';
+import "./App.scss";
+
+
+
+const Home = () => {
+  return (
+        <h2>I'M THE HOME PAGE</h2>
+  );
+};
+
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      books: [],
-      userInput: "",
-      bookShelf: [],
-      readingList: [],
-    };
-  }
 
-  componentDidMount() {
-    // For retrieval of bookshelf list***
-    const dbRef = firebase.database().ref("readingList");
-    dbRef.on("value", (snapshot) => {
-      const readingList = [];
-      const data = snapshot.val();
-
-      for (let key in data) {
-        readingList.push({
-          id: key,
-          bookList: data[key],
-        });
-      }
-      this.setState({
-        bookShelf: readingList,
-      });
-      console.log(this.state.bookShelf);
-    });
-  }
-
-  findBooks = (searchItem) => {
-    axios({
-      url: `https://www.googleapis.com/books/v1/volumes?`,
-      method: "GET",
-      responseType: "JSON",
-      params: {
-        key: "AIzaSyD7Ytli5GUZu5S7FoaFn-sSMzsdWuwv_8E",
-        q: searchItem,
-      },
-    }).then((response) => {
-      let books = response.data.items;
-      this.setState({
-        books,
-	  });
-
-    });
-  };
-
-  handleChange = (event) => {
-    this.setState({
-      userInput: event.target.value,
-    });
-  };
-
-  handleClick = () => {
-    if (!this.state.userInput) return;
-    let searchTerm = this.state.userInput;
-    this.findBooks(searchTerm);
-    this.setState({
-      userInput: "",
-    });
-  };
-
-  handleClickAdd = (index) => {
-    const dbRef = firebase.database().ref("readingList");
-    console.log(index);
-    const objectPush = {
-      title: this.state.books[index].volumeInfo.title,
-      author: this.state.books[index].volumeInfo.authors,
-      description: this.state.books[index].volumeInfo.description,
-      genre: this.state.books[index].volumeInfo.categories,
-      rating: this.state.books[index].volumeInfo.averageRating,
-      imageLinks: this.state.books[index].volumeInfo.imageLinks,
-	};
-	
-	for(let item in objectPush){
-		if(objectPush[item] === undefined){
-			 item = null;
-			 console.log(objectPush[item])
-		};
-	}
-
-    this.setState({
-      readingList: objectPush,
-    });
-    dbRef.push(objectPush);
-  };
-
-  handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      if (!this.state.userInput) return;
-      let searchTerm = this.state.userInput;
-      this.findBooks(searchTerm);
-      this.setState({
-        userInput: "",
-      });
     }
   };
 
   render() {
+  
     return (
-      <div className="App">
-        <header>
-          <h1>Page Turners</h1>
-        </header>
-        <input
-          type="text"
-          value={this.state.userInput}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-          name="userInput"
-        />
-        <button onClick={this.handleClick}>Search Book</button>
+      <Router>
+        <div className="App">
 
-        <h1>Books!</h1>
-        <ul>
-          {this.state.books.map((book, index) => {
-			  
-            // let searchedBook = book.volumeInfo;
-            return (
-              <li key={index}>
-                <BookItem
-                  key={index}
-                  title={book.volumeInfo.title}
-                  authors={book.volumeInfo.authors}
-                  description={book.volumeInfo.description}
-                  genre={book.volumeInfo.categories}
-                  rating={book.volumeInfo.averageRating}
-                //   thumbnail={book.volumeInfo.imageLinks.thumbnail}
-                //   imageLinks={book.volumeInfo.imageLinks}
-                  handleClickAdd={this.handleClickAdd}
-                />
-                <button onClick={() => this.handleClickAdd(index)}>
-                  Add Book
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+          <header>
+            <h1>Page Turners</h1>
+
+            <nav className="headerNav">
+              <ul> 
+                <li>
+                  <Link to="/">Landing</Link>
+                </li> 
+                <li>
+                  <Link to="/home">Home</Link> 
+                </li>
+                <li>
+                  <Link to="/account">My Reading Lists</Link>
+                </li> 
+              </ul>
+          </nav> 
+
+          </header>
+          
+          <main>
+            <div className="pageReturn">
+
+              <Route exact path="/" component={Welcome} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/account" component={Account} />
+
+            </div>
+
+            <div className="pageReturnAside">
+
+              <h3>I AM A TITLE</h3>
+              <p>Hello</p>
+
+            </div>
+
+          </main>
+
+          <footer>
+            <div className="groupMembers">
+              <h3>Group Project By:</h3>
+              <ul>
+                <li>
+                  <img src={githubLogo} alt="Github Logo"/>
+                  <a href="https://github.com/daibhidhdwaum">@daibhidhdwaum</a>
+                </li>
+                <li>
+                  <img src={githubLogo} alt="Github Logo"/>
+                  <a href="https://github.com/vigyan-k">@vigyan-k</a> 
+                </li>
+                <li>
+                  <img src={githubLogo} alt="Github Logo"/>
+                  <a href="https://github.com/OksanaSam">@OksanaSama</a>
+                </li>
+                <li>
+                  <img src={githubLogo} alt="Github Logo"/>
+                  <a href="https://github.com/amay-zingg">@amay-zingg</a>
+                </li>
+              </ul>
+            </div>
+            </footer>
+          
+        </div>
+        </Router>
+          
     );
-  }
+  };
 }
+
 export default App;
---
+++ src/components/landing.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+
+const Welcome = () => {
+  return (
+      <Fragment>
+        <h2>I'M A LANDING PAGE</h2>
+        <h2>Welcome</h2>
+        <p>Hello! Find the books you want to read using Page Turners and keep your list as a reference.</p>
+        </Fragment>
+  );
+};
+
+export default Welcome;
