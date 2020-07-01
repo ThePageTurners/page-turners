ab53e9de1a0bceebf30e0d6e926d057691fc6e01
amay-zingg <anastasiamay29@gmail.com>
2020-07-01 19:23:34 -0400

    Working on nav also colour/font change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import firebase from './Firebase/index.js';
+import Navigation from './components/Navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import faGithub from './assets/github.svg';
+import './App.scss';
 
 // * * * * ROUTER ELEMENTS
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -8,66 +13,63 @@ import Search from './components/Search';
 import Bookshelf from './components/Bookshelf';
 // * * * * ROUTER ELEMENTS END
 
-import githubLogo from './assets/github.svg';
-import './App.scss';
-
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			user: {},
 			isLoggedIn: false,
-      		userNames: "",
-      		activeUser: "",
+			userNames: '',
+			activeUser: ''
 		};
-	  }
+	}
 
+	github = <FontAwesomeIcon icon={faGithub} size="1x"/>;
 
-	  componentDidMount() {
+	componentDidMount() {
 		const dbRef = firebase.database().ref();
 		dbRef.once('value', (response) => {
 			const data = response.val();
-      		const newState = Object.keys(data);
-      		console.log(data);
-      		console.log(newState);
+			const newState = Object.keys(data);
+			console.log(data);
+			console.log(newState);
 			this.setState({
 				userNames: newState
-			})
-		})
-	  }
-
-	  handleChangeUser = (event) => {
-		this.setState({
-		  activeUser: event.target.value
+			});
 		});
-	  };
+	}
 
+	handleChangeUser = (event) => {
+		this.setState({
+			activeUser: event.target.value
+		});
+	};
 
-	  handleLogin = (event) => {
-		if (this.state.activeUser !== "") {
-		  event.preventDefault();
-		  const userNames = this.state.userNames;
-		  const userName = this.state.activeUser;
-		  console.log(userName);
-		  const dbRef = firebase.database().ref(this.state.activeUser);
-		  console.log(dbRef);
+	handleLogin = (event) => {
+		if (this.state.activeUser !== '') {
+			event.preventDefault();
+			const userNames = this.state.userNames;
+			const userName = this.state.activeUser;
+			console.log(userName);
+			const dbRef = firebase.database().ref(this.state.activeUser);
+			console.log(dbRef);
 			if (userNames.includes(userName)) {
-				alert("Hello again!")
+				alert('Hello again!');
 			} else {
-				alert("Welcome, new user!")
+				alert('Welcome, new user!');
 				//creating an object in firebase with userName as a key
-				dbRef.set([1, 2, 3])
+				dbRef.set([ 1, 2, 3 ]);
 			}
-	
-		  //removing login form from the UI
-		  this.setState({
-			isLoggedIn: true
-		  })
-		} else {
-		  alert("Please enter your name!")
-		};
-	  };
 
+			//removing login form from the UI
+			this.setState({
+				isLoggedIn: true
+			});
+		} else {
+			alert('Please enter your name!');
+		}
+		
+	};
 
 	render() {
 		return (
@@ -75,15 +77,26 @@ class App extends Component {
 				<div className="App">
 					<header>
 						{/* LOGIN FORM */}
-						{ (this.state.isLoggedIn === false) ? 
-						(<div>
-							<form className = "login">
-								<input aria-label="username" type="text" placeholder="Username" value={this.state.activeUser} onChange={this.handleChangeUser} required/>
-								<button type="submit" onClick={this.handleLogin}>Login</button>
-							</form>
-						</div>) : null }
+						{/* {this.state.isLoggedIn === false ? (
+							<div>
+								<form className="login">
+									<input
+										aria-label="username"
+										type="text"
+										placeholder="Username"
+										value={this.state.activeUser}
+										onChange={this.handleChangeUser}
+										required
+									/>
+									<button type="submit" onClick={this.handleLogin}>
+										Login
+									</button>
+								</form>
+							</div>
+						) : null} */}
 
-						<h1>Page Turners</h1>
+						{/* Navigation Before */}
+						{/* <h1>Page Turners</h1>
 
 						<nav className="headerNav">
 							<ul>
@@ -97,16 +110,18 @@ class App extends Component {
 									<Link to="/bookshelf">My Bookshelf</Link>
 								</li>
 							</ul>
-						</nav>
+						</nav> */}
+
+						<Link to="/" className="pageTitle"><h1>Page Turners</h1></Link>
+						<Navigation />
 					</header>
 
-
-          <main>
-            <div>
-              	<Route exact path="/" component={Home} />
-              	<Route exact path="/search" component={Search} />
-				<Route exact path="/bookshelf" component={Bookshelf} />
-            </div>
+					<main>
+						<div>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/search" component={Search} />
+							<Route exact path="/bookshelf" component={Bookshelf} />
+						</div>
 
 						{/* <div className="pageReturnAside">
               <h3>I AM A TITLE</h3>
@@ -119,20 +134,16 @@ class App extends Component {
 							<h3>Group Project By:</h3>
 							<ul>
 								<li>
-									<img src={githubLogo} alt="Github Logo" />
-									<a href="https://github.com/daibhidhdwaum">@daibhidhdwaum</a>
+									<a href="https://github.com/daibhidhdwaum">{this.github} @daibhidhdwaum</a>
 								</li>
 								<li>
-									<img src={githubLogo} alt="Github Logo" />
-									<a href="https://github.com/vigyan-k">@vigyan-k</a>
+									<a href="https://github.com/vigyan-k">{this.github} @vigyan-k</a>
 								</li>
 								<li>
-									<img src={githubLogo} alt="Github Logo" />
-									<a href="https://github.com/OksanaSam">@OksanaSam</a>
+									<a href="https://github.com/OksanaSam">{this.github} @OksanaSam</a>
 								</li>
 								<li>
-									<img src={githubLogo} alt="Github Logo" />
-									<a href="https://github.com/amay-zingg">@amay-zingg</a>
+									<a href="https://github.com/amay-zingg">{this.github} @amay-zingg</a>
 								</li>
 							</ul>
 						</div>
