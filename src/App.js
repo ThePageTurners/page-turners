1debc7d1b15d1b4ca7c5b1de02079fb249a07401
oksana sam <oksana.o.samokhvalova@gmail.com>
2020-07-01 13:28:27 -0400

    added firebase to main file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from './Firebase/index.js';
 
 // * * * * ROUTER ELEMENTS
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
